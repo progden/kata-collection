0558d5a79777b706268572ff5782f60a2eda489c
progden <ccdrumer@gmail.com>
2026-01-11 20:17:21 +0800

    [refactor], [feature] Refactor `updateQuality` for modularity and readability
    重構 `updateQuality` 方法，分解商品邏輯處理至專屬方法以提升可維護性。
    新增商品類型判斷與品質與保質期操作的輔助方法，簡化核心邏輯的可讀性與結構化。

--- GildedRose-Refactoring/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose-Refactoring/src/main/java/com/gildedrose/GildedRose.java
@@ -6,58 +6,95 @@ class GildedRose {
     public GildedRose(Item[] items) {
         this.items = items;
     }
-
+    
     // updateQuality 介面不可以修改！！！！
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+            Item item = items[i];
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+            // AgedBrieItemWorkflow
+            if (isAgedBrie(item)) {
+                processQuality(item, 1);
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
+                decreaseSellIn(item, -1);
+
+                if (isOutofSellIn(item)) {
+                    processQuality(item, 1);
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
+            // isSulfurasItemWorkflow
+            if (isSulfuras(item)) {
+                processQuality(item, 1);
+                decreaseSellIn(item, 0);
+
+                if (isOutofSellIn(item)) {
+                    continue;
+                }
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+            // BackstagePassItemWorkflow
+            if (isBackstagePass(item)) {
+                processQuality(item, 1);
+                if (item.sellIn < 11) {
+                    if (item.quality < 50) {
+                        processQuality(item, 1);
                     }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                }
+                if (item.sellIn < 6) {
+                    if (item.quality < 50) {
+                        processQuality(item, 1);
                     }
+                }
+                decreaseSellIn(item, -1);
+                if (isOutofSellIn(item)) {
+                    processQuality(item, -item.quality);
+                }
+            }
+
+            if (isNormalProduct(item)) {
+                processQuality(item, -1);
+                decreaseSellIn(item, -1);
+                if (isOutofSellIn(item)) {
+                    processQuality(item, -1);
                 }
             }
         }
+    }
+
+    private static boolean isOutofSellIn(Item item) {
+        return item.sellIn < 0;
+    }
+
+    private static void decreaseSellIn(Item item, int i) {
+        item.sellIn = item.sellIn + i;
+    }
+
+    private static void processQuality(Item item, int i) {
+        if (i < 0 && item.quality > 0) {
+            item.quality = item.quality + i;
+        }
+
+        if (i > 0 && item.quality < 50) {
+            item.quality = item.quality + 1;
+        }
+    }
+
+    private static boolean isNormalProduct(Item item) {
+        return !isAgedBrie(item)
+                && !isBackstagePass(item)
+                && !isSulfuras(item);
+    }
+
+    private static boolean isSulfuras(Item item) {
+        return item.name.equals("Sulfuras, Hand of Ragnaros");
+    }
+
+    private static boolean isBackstagePass(Item item) {
+        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
+    }
+
+    private static boolean isAgedBrie(Item item) {
+        return item.name.equals("Aged Brie");
     }
 }
